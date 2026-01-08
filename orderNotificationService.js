9bd06db1c7630a4c23b7c1b5240af2384ebab638
Suprodip <suprodipbhattacharjee1@gmail.com>
2026-01-08 18:08:50 +0530

    implemented the exchange type 'topic' in this code block

---
+++ orderNotificationService.js
@@ -0,0 +1,36 @@
+import amqp from "amqplib";
+
+const recieveMessages = async () => {
+  try {
+    const connection = await amqp.connect("amqp://localhost");
+    const channel = await connection.createChannel();
+
+    const exchange = "notification_exchange";
+    const queue = "order_queue";
+
+    await channel.assertExchange(exchange, "topic", { durable: true });
+    await channel.assertQueue(queue, { durable: true });
+
+    await channel.bindQueue(queue, exchange, "order.*");
+
+    console.log("Waiting for messages");
+    channel.consume(
+      queue,
+      (msg) => {
+        if (msg !== null) {
+          console.log(
+            `[Order Notification] Msg was consumed! with routingkey ${
+              msg.fields.routingKey
+            } and content as ${msg.content.toString()}`
+          );
+          channel.ack(msg);
+        }
+      },
+      { noAck: false }
+    );
+  } catch (error) {
+    console.log("Error:", error);
+  }
+};
+
+recieveMessages();
